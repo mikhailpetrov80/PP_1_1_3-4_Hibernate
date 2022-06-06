0a7f327a4e61d7c2ea051f70945ee6a0d95da043
Михаил Петров <mikhailpetrov80@gmail.com>
2022-06-07 01:51:40 +0400

    Task PP_1.1.4 correction  06.06.20022 commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -16,74 +16,87 @@ public class UserDaoHibernateImpl implements UserDao {
     private static final String dropUsersTableSql = "DROP TABLE IF EXISTS USERS";
     private static final String cleanUsersTableSql = "TRUNCATE TABLE USERS";
 
+    Session session = Util.getSessionFactory().openSession();
+
     @Override
     public void createUsersTable() {
-        Transaction transaction;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Transaction transaction = null;
+        try {
             transaction = session.beginTransaction();
             session.createNativeMutationQuery(createUsersTableSql).executeUpdate();
             transaction.commit();
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 
     @Override
     public void dropUsersTable() {
-        Transaction transaction;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Transaction transaction = null;
+        try {
             transaction = session.beginTransaction();
             session.createNativeMutationQuery(dropUsersTableSql).executeUpdate();
             transaction.commit();
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
-        Transaction transaction;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Transaction transaction = null;
+        try {
             transaction = session.beginTransaction();
             session.persist(user);
             transaction.commit();
             System.out.printf("User с именем – %s добавлен в базу данных\n", name);
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Transaction transaction;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Transaction transaction = null;
+        try {
             transaction = session.beginTransaction();
             session.remove(session.get(User.class, id));
             transaction.commit();
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Transaction transaction;
-        List<User> users = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            users = session.createQuery("from User", User.class).list();
-            transaction.commit();
-            System.out.println(users);
-        } catch (Exception ignored) {
-        }
-        return users;
+        return session.createQuery("From User", User.class).list();
     }
 
     @Override
     public void cleanUsersTable() {
-        Transaction transaction;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Transaction transaction = null;
+        try {
             transaction = session.beginTransaction();
             session.createNativeMutationQuery(cleanUsersTableSql).executeUpdate();
             transaction.commit();
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 }
